596331a7d3b8e89668fde384205795ffe7e92ae3
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2025-06-27 00:07:19 +0900

    Python code edded
    Signed-off-by: Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>

--- main.py
+++ main.py
@@ -359,7 +359,6 @@ def admin_db():
             print("Admin user already exists.")
 
 asgi_app = WsgiToAsgi(app)
-
 # if __name__ == '__main__':
 #     with app.app_context():
 #         db.create_all()
